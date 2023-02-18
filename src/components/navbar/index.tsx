23e22997743bc717aaad48d8c3627da23135b4f0
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-18 13:00:45 -0300

    navbar development

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,12 +1,11 @@
 export default function Navbar(){
     return(
         <div>
-            <img src="" alt="" />
+            <img className="w-44 mr-auto ml-auto" src="/img/cybertechbrasil.png" alt="" />
             <nav>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                <ul className="flex justify-around mt-5 mb-5">
+                    <li>Início</li>
+                    <li>Contato</li>
                 </ul>
             </nav>
         </div>
