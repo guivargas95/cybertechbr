9c25ea0ce3cb24e9f2817301a27299864b8e15c0
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-18 12:50:40 -0300

    Initial architecture and files

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return(
+        <div>
+            <img src="" alt="" />
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,9 @@
-export default function Home(){
-    return(
-        <div className="text-center text-blue-400">
-            <h1>Welcome to Cyber Tech Brasil</h1>
+import Navbar from "../../components/navbar";
+
+export default function Home() {
+    return (
+        <div className="">
+            <Navbar/>
         </div>
     )
 }
