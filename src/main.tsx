0f7da50f47bf2c4d2d61c58fd86d89d27e900d9d
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-20 17:17:30 -0300

    function to scroll to top added

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './config.css'
+import ArticlePage from './pages/articlePage';
 import Home from './pages/home';
+import ScrollToTop from './components/scrollToTop';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/article/:id" element={<ArticlePage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
