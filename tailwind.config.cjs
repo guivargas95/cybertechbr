1a57831d298239257e697508a395587bc038f31b
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-20 17:16:58 -0300

    fonts added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,16 +5,23 @@ module.exports = {
     theme: {
         extend: {
             width: {
+                '150': '80rem',
                 '140': '50rem',
                 '128': '32rem',
                 '112': '28rem',
             },
 
             height: {
+                '150': '80rem',
+                '140': '50rem',
                 '128': '32rem',
                 '112': '28rem',
 
             },
+        },
+        fontFamily: {
+            texts: ['Inter', 'sans-serif'],
+            titles: ['Gloock', 'serif'],
         },
     },
     plugins: [],
