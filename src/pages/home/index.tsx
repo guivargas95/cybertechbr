27df36a78d8fdfbc1d7d679ee617498d51f43e43
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-20 17:18:56 -0300

    fonts added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div>
             <Navbar />
-            <h1 className="text-center text-3xl text-gray-500 mt-5 mb-5">Artigos de tecnologia</h1>
+            <h1 className="text-center text-3xl text-gray-500 mt-5 mb-5 font-bold">Artigos de tecnologia</h1>
             <HighLightsArticles id1={1} id2={2} id3={3} id4={4}/>
             <AllArticles />
         </div>
