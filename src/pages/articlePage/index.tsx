1156f97b5b3200dcc1c5db811043b1a48a8c1900
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-21 11:36:43 -0300

    text css changed

--- src/pages/articlePage/index.tsx
+++ src/pages/articlePage/index.tsx
@@ -39,24 +39,24 @@ export default function ArticlePage() {
                                 <div className="w-10/12 md:w-6/12 mr-auto ml-auto border-t border-b border-gray-400">
                                     <h1 className="text-center mt-5 mb-5 text-2xl font-titles">{response.title}</h1>
                                     <div className="text-left font-texts font-bold">
-                                        <p className="mt-2 mb-2 text-left font-texts font-bold">{response.p1}</p>
+                                        <p className="">{response.p1}</p>
                                     </div>
                                     <img className="lg:h-128 lg:w-150 mt-10 mb-10 md:mr-auto md:ml-auto" src={`${imgPath}/${response.img1}`} alt={response.alt1} />
                                     <div>
-                                        <ul>
-                                            <li>{response.tip1}</li>
-                                            <li>{response.tip2}</li>
-                                            <li>{response.tip3}</li>
-                                            <li>{response.tip4}</li>
-                                            <li>{response.tip5}</li>
-                                            <li>{response.tip6}</li>
-                                            <li>{response.tip7}</li>
-                                            <li>{response.tip8}</li>
-                                            <li>{response.tip9}</li>
-                                            <li>{response.tip10}</li>
+                                        <ul className="text-left font-texts font-bold">
+                                            <li className="mt-2 mb-2">{response.tip1}</li>
+                                            <li className="mt-2 mb-2">{response.tip2}</li>
+                                            <li className="mt-2 mb-2">{response.tip3}</li>
+                                            <li className="mt-2 mb-2">{response.tip4}</li>
+                                            <li className="mt-2 mb-2">{response.tip5}</li>
+                                            <li className="mt-2 mb-2">{response.tip6}</li>
+                                            <li className="mt-2 mb-2">{response.tip7}</li>
+                                            <li className="mt-2 mb-2">{response.tip8}</li>
+                                            <li className="mt-2 mb-2">{response.tip9}</li>
+                                            <li className="mt-2 mb-2">{response.tip10}</li>
                                         </ul>
-                                        <div className="text-left font-texts font-bold">
-                                            <p className="mt-2 mb-2 text-left font-texts font-bold">{response.p3}</p>
+                                        <div className="mt-10 mb-10 text-left font-texts font-bold">
+                                            <p className="">{response.p3}</p>
                                         </div>
                                     </div>
                                 </div>
