a4c0c891369baeab0ddde341731a54373de56d3e
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-23 20:20:15 -0300

    new article added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,8 +7,8 @@ export default function Home() {
         <div className="mt-32">
             <Navbar />
             <h1 className="text-center text-3xl text-gray-500 mt-5 mb-5 font-bold">Artigos de tecnologia</h1>
-            <HighLightsArticles id1={15} id2={19} id3={20} id4={21}/>
-            <AllArticles id1={15} id2={19} id3={20} id4={21}/>
+            <HighLightsArticles id1={10} id2={19} id3={20} id4={21} />
+            <AllArticles id1={10} id2={19} id3={20} id4={21} />
         </div>
     )
 }
