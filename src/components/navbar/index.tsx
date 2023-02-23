c7217cb2f2ca72d90a6293019b210d905e313d13
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-23 13:30:41 -0300

    some navbar tests

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <div>
-            <Link to="/"><img className="w-44 mt-3 mb-3 mr-auto ml-auto" src="/img/cybertechbrasil.png" alt="" /></Link>
+            <Link to="/"><img className="w-44 mt-3 mb-3 mr-auto ml-auto" src="/img/cybertechbrasil.png" alt="Logo da CyberTech Brasil" /></Link>
             <nav>
                 <ul className="flex justify-around mt-10 mb-10">
                     <Link to="/"><li>Início</li></Link>
+                    
                 </ul>
             </nav>
         </div>
