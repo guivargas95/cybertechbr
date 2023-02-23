f5375b5933102dac51cf9b7cb2f3eddc30caafeb
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-23 19:47:47 -0300

    design of navbar changed

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,12 +3,19 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <div>
-            <Link to="/"><img className="w-44 mt-3 mb-3 mr-auto ml-auto" src="/img/cybertechbrasil.png" alt="Logo da CyberTech Brasil" /></Link>
-            <nav>
-                <ul className="flex justify-around mt-10 mb-10">
-                    <Link to="/"><li>Início</li></Link>
-                    
-                </ul>
+            <nav className="bg-blue-500 p-2 mt-0 fixed w-full z-10 top-0 md:h-20">
+                <div className="container mx-auto flex flex-wrap items-center">
+                    <div className="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
+                        <a className="text-white no-underline hover:text-white hover:no-underline" href="#">
+                            <img className="w-40 h-16 mr-auto ml-auto" src="/img/cybertechbrasil.png" alt="Logo da CyberTech Brasil" />
+                        </a>
+                    </div>
+                    <div className="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
+                        <ul className="list-reset flex justify-center flex-1 md:flex-none items-center lg:justify-between lg:mr-36">
+                            <li className="font-titles font-bold text-lg hover:text-gray-700"><Link to="/">Início</Link></li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </div>
     )
