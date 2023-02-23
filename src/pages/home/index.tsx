51eeb7fd681fdf9b531501916689d62defeb607b
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-23 19:47:55 -0300

    responsive fixes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ import Navbar from "../../components/navbar";
 
 export default function Home() {
     return (
-        <div>
+        <div className="mt-32">
             <Navbar />
             <h1 className="text-center text-3xl text-gray-500 mt-5 mb-5 font-bold">Artigos de tecnologia</h1>
             <HighLightsArticles id1={15} id2={19} id3={20} id4={21}/>
