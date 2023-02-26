6ca4d961ce9263e12fe675d276b10b2afe43d0cb
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-26 10:16:21 -0300

    navbar responsive fix

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,14 +4,12 @@ export default function Navbar() {
     return (
         <div>
             <nav className="bg-blue-500 p-2 mt-0 fixed w-full z-10 top-0 md:h-20">
-                <div className="container mx-auto flex flex-wrap items-center">
-                    <div className="flex w-full md:w-1/2 justify-center md:justify-between  text-white font-extrabold">
-                        <a className="text-white no-underline hover:text-white hover:no-underline" href="#">
-                            <img className="w-40 h-16 mr-auto ml-auto" src="/img/cybertechbrasil.png" alt="Logo da CyberTech Brasil" />
-                        </a>
+                <div className="md:flex md:justify-around">
+                    <div className="text-white font-extrabold">
+                        <Link to="/"><img className="w-40 h-16 mr-auto ml-auto" src="/img/cybertechbrasil.png" alt="Logo da CyberTech Brasil" /></Link>
                     </div>
-                    <div className="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
-                        <ul className="list-reset flex justify-center flex-1 md:flex-none items-center md:justify-between">
+                    <div className="">
+                        <ul className="text-center mt-3 mb-3">
                             <li className="font-titles font-bold text-lg hover:text-gray-700"><Link to="/">Início</Link></li>
                         </ul>
                     </div>
