a53e72d57c582088b35287ab92fa93e9f04b71c6
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-28 20:11:22 -0300

    changed color of category

--- src/components/articles/allArticles/index.tsx
+++ src/components/articles/allArticles/index.tsx
@@ -27,7 +27,7 @@ export default function AllArticles({ id1, id2, id3, id4 }: iArticleId) {
                             <div className="w-80 md:w-96 mt-3 mb-3 md:mt-6 md:mb-6 md:mr-auto md:ml-auto md:h-112" key={response.id}>
                                 <Link to={`/article/${response.id}`}><h2 className="text-xl text-left font-bold md:h-16 font-titles">{response.title}</h2></Link>
                                 <Link to={`/article/${response.id}`}><img className="w-full h-56" src={`${imgPath}/${response.img1}`} alt={response.alt1} /></Link>
-                                <h3 className="text-blue-400 mt-2 mb-2 font-bold">{response.category}</h3>
+                                <h3 className="text-blue-800 mt-2 mb-2 font-bold">{response.category}</h3>
                                 <p className="text-left font-texts font-bold text-sm">{`${response.p1.substring(0, 300)}...`}</p>
                             </div>
                         )
