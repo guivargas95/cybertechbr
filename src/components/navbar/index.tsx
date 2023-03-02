8310ba4b9c3c7ee40ecc7595f5a49cfc26f6a0fb
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-02 11:49:51 -0300

    included category to navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <div>
-            <nav className="bg-blue-500 p-2 mt-0 fixed w-full z-10 top-0 md:h-20">
+            <nav className="bg-blue-500 p-2 mt-0 fixed w-full z-10 top-0 md:h-32">
                 <div className="md:flex md:justify-around">
                     <div className="text-white font-extrabold">
                         <Link to="/"><img className="w-40 h-16 mr-auto ml-auto" src="/img/cybertechbrasil.png" alt="Logo da CyberTech Brasil" /></Link>
@@ -14,7 +14,13 @@ export default function Navbar() {
                         </ul>
                     </div>
                 </div>
+                <ul className="flex justify-center mt-5 text-xl">
+                    <Link to="/category/Tecnologia"><li className="ml-5 mr-5">Tecnologia</li></Link>
+                    <Link to="/category/Saúde"><li className="ml-5 mr-5">Saúde</li></Link>
+                    <Link to="/category/Meio%20Ambiente"><li className="ml-5 mr-5">Meio Ambiente</li></Link>
+                </ul>
             </nav>
+
         </div>
     )
 }
