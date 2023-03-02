d04bb6bb0d4514e259ff3a2330d5cb6f05ca7932
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-02 16:16:54 -0300

    new article added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
             <AdsTerra keyAd={'8b60dd6d58076a1bb31d3ef53b33b8eb'} width={320} height={50} />
             <h1 className="text-center text-3xl text-gray-500 mt-5 mb-5 font-bold">Artigos e as melhores dicas para você!</h1>
             <HighLightsArticles id1={35} id2={32} id3={31} id4={34} />
+            <AdsTerra keyAd={'63d2ae3470ce030358178c5721040be3'} width={300} height={250} />
             <AllArticles id1={35} id2={32} id3={31} id4={34} />
             {posthog.has_opted_in_capturing() || posthog.has_opted_out_capturing() ? null : <CookiePolicyBanner />}
             <Footer />
