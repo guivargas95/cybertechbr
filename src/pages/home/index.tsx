56db11f172275f7b304bfe3cc9beacf7a255556c
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-07 18:18:17 -0300

    new article added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -16,9 +16,9 @@ export default function Home() {
             <Navbar />
             <AdsTerraComponent />
             <h1 className="text-center text-3xl text-gray-500 mt-5 mb-5 font-bold">Artigos e as melhores dicas para você!</h1>
-            <HighLightsArticles id1={35} id2={32} id3={31} id4={34} />
+            <HighLightsArticles id1={39} id2={32} id3={31} id4={34} />
             <AdsTerraComponent />
-            <AllArticles id1={35} id2={32} id3={31} id4={34} />
+            <AllArticles id1={39} id2={32} id3={31} id4={34} />
             {posthog.has_opted_in_capturing() || posthog.has_opted_out_capturing() ? null : <CookiePolicyBanner />}
             <AdsTerraComponent />
             <Footer />
